56b92076d886666463a8572930fd60210b3de850
ScarsIntoStars <conpana_jj@naver.com>
2023-09-08 11:31:42 +0900

    02. DTO 필드 생성

--- src/main/java/com/icia/member/dto/MemberDTO.java
+++ src/main/java/com/icia/member/dto/MemberDTO.java
@@ -4,4 +4,10 @@ import lombok.Data;
 
 @Data
 public class MemberDTO {
+    public Integer id;
+    public String memberEmail;
+    public String memberPassword;
+    public String memberName;
+    public String memberBirth;
+    public String memberMobile;
 }
